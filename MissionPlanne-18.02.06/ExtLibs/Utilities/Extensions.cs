0ca1d6c00f5155cb63a2e38d56853a9d4d342502
caoshiyue <caoshiyueKevin@Gmail.com>
2018-05-11 17:36:38 +0800

    upload Mission Planner

---
+++ MissionPlanne-18.02.06/ExtLibs/Utilities/Extensions.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MissionPlanner.Utilities
+{
+    public static class Extensions
+    {
+        public static string TrimUnPrintable(this string input)
+        {
+            return Regex.Replace(input, @"[^\u0020-\u007E]", String.Empty);
+        }
+
+        public static async void Async(this Action function)
+        {
+            await Task.Run(() => { function(); });
+        }
+
+        public static async Task<TOut> Async<TOut>(this Func<TOut> function)
+        {
+            return await Task.Run(() => { return function(); });
+        }
+
+        public static async Task<TOut> Async<TIn, TOut>(this Func<TIn, TOut> function, TIn input)
+        {
+            return await Task.Run(() => { return function(input); });
+        }
+
+        public static void Add<T, T2>(this List<Tuple<T, T2>> input, T in1, T2 in2)
+        {
+            input.Add(new Tuple<T, T2>(in1, in2));
+        }
+
+        public static void Add<T, T2, T3>(this List<Tuple<T, T2, T3>> input, T in1, T2 in2, T3 in3)
+        {
+            input.Add(new Tuple<T, T2, T3>(in1, in2, in3));
+        }
+
+        public static bool IsNumber(this object value)
+        {
+            if (Equals(value, null))
+            {
+                return false;
+            }
+
+            return value is sbyte
+                   || value is byte
+                   || value is short
+                   || value is ushort
+                   || value is int
+                   || value is uint
+                   || value is long
+                   || value is ulong
+                   || value is float
+                   || value is double
+                   || value is decimal;
+        }
+    }
+}
---
+++ MissionPlanne-18.02.06/Mavlink/Extension.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Forms;
+using MissionPlanner;
+using MissionPlanner.Controls;
+using MissionPlanner.Utilities;
+using Newtonsoft.Json;
+
+public static class Extension
+{
+    public static string ToJSON(this MAVLink.MAVLinkMessage msg)
+    {
+        return JsonConvert.SerializeObject(msg);
+    }
+
+    public static MAVLink.MAVLinkMessage FromJSON(this string msg)
+    {
+        return JsonConvert.DeserializeObject<MAVLink.MAVLinkMessage>(msg);
+    }
+}
